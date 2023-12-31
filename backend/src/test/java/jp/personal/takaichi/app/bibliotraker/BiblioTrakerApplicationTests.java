f2ad54896251f53a449a449f6629b6063e284798
Takazu8108180 <takaichi.0818.tech@gamil.com>
2023-12-31 22:15:33 +0900

    create: springbootプロジェクト作成

---
+++ backend/src/test/java/jp/personal/takaichi/app/bibliotraker/BiblioTrakerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.personal.takaichi.app.bibliotraker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioTrakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
